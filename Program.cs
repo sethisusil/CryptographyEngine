74661e75aa16b83be537944a60cb4e7329c18155
sethisusil <sethisusil@gmail.com>
2017-11-28 17:28:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "+7bzHgG+ftsEws4oWFCg8sIMKz+/KiA7";
+            string decryptPassword = password.Decrypt();
+            Console.WriteLine("Decrypted Name:-   {0}", decryptPassword);
+            Console.Read();
+        }
+    }
+}
