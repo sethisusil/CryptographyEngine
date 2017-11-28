d1f7be0a86087433892af3b47036adcc9382319b
sethisusil <sethisusil@gmail.com>
2017-11-28 17:27:10 +0530

    Add files via upload

---
+++ CryptoEngine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace EncryptDecrypt
+{
+    public static class CryptoEngine
+    {
+       
+        public static string Key
+        {
+            get
+            {
+                string _key= "adfvblmewyign#$%";
+                return _key;
+            }
+            
+        }
+        public static string Encrypt(this string input)
+        {
+            byte[] inputArray = Encoding.UTF8.GetBytes(input);
+            TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+               
+            };
+            ICryptoTransform cTransform = tripleDes.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public static string Decrypt(this string input)
+        {
+            byte[] inputArray = Convert.FromBase64String(input);
+            TripleDESCryptoServiceProvider tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            ICryptoTransform cTransform = tripleDes.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);
+            tripleDes.Clear();
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
